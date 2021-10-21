c6cc3ec8fbc117ee2bd405977a2d5472cec715d9
hugo-cerezo <55873067+hugo-cerezo@users.noreply.github.com>
2021-10-21 07:34:03 +0200

    map generated --> step 3 of tutorial
    run main.py
    
    use arrows to move white sprite

--- main.py
+++ main.py
@@ -1,38 +1,59 @@
-import room
+#!/usr/bin/env python3
+import tcod
 
-class Entity:
-    """
-    A generic object to represent players, enemies, items, etc.
-    """
-    def __init__(self, x, y, char, color, name, inventory, blocks=False, fighter=None, ai=None):
-        self.x = x
-        self.y = y
-        self.char = char
-        self.color = color
-        self.name = name
-        self.blocks = blocks
-        self.fighter = fighter
-        self.ai = ai
-        self.inventory = inventory
+from engine import Engine
+from entity import Entity
+from input_handlers import EventHandler
+from procgen import generate_dungeon
 
-        if self.fighter:
-            self.fighter.owner = self
 
-        if self.ai:
-            self.ai.owner = self
+def main() -> None:
+    screen_width = 180
+    screen_height = 95
 
-class Warior:
-    def __init__(self, hp, defense, power,mana):
-        self.max_hp = hp
-        self.hp = hp
-        self.defense = defense
-        self.power = power
-        self.mana = mana
+    map_width = 180
+    map_height = 95
 
-class Mage:
-    def __init__(self, hp, defense, power,mana):
-        self.max_hp = hp
-        self.hp = hp
-        self.defense = defense
-        self.power = power
-        self.mana = mana
+    room_max_size = 20
+    room_min_size = 15
+    max_rooms = 30
+
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    event_handler = EventHandler()
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player
+    )
+
+    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
+
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title="Yet Another Roguelike Tutorial",
+        vsync=True,
+    ) as context:
+        root_console = tcod.Console(screen_width, screen_height, order="F")
+        while True:
+            engine.render(console=root_console, context=context)
+
+            events = tcod.event.wait()
+
+            engine.handle_events(events)
+
+
+if __name__ == "__main__":
+    main()
